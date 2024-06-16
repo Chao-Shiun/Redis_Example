a8f4598523099a51c07d8cc858830a8f11457c8d
Charles <e4366.tw@gmail.com>
2024-06-16 22:29:53 +0800

    Updated dependencies for Redis_Example project.

--- WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46d48b3d845acbf6322b0da61e66e7b1faecbd70")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f802cb6bd869c4556ed69b1c53261f7b7ec49c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
